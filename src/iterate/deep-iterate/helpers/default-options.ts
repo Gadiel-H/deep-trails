1a5da37f8a2647dcdf4662b37327de9e313de659
Gadiel Hernández <gadiel.dev@proton.me>
2026-01-31 01:35:32 -0700

    fix: also report unexpected properties via proxy

--- src/iterate/deep-iterate/helpers/default-options.ts
+++ src/iterate/deep-iterate/helpers/default-options.ts
@@ -5,6 +5,7 @@ import { toSimpleString } from "../../../utils/public/index.js";
 import { optionsSchema } from "../schemas/options-schema.js";
 
 const optionKeys = new Set(Reflect.ownKeys(optionsSchema));
+const optionsList = [...optionKeys].map(String).join("\n      ");
 
 /** Default options argument for `deepIterate`. @internal */
 export const defaultOptions = new Proxy<Options<any, any, any>>(
@@ -21,7 +22,11 @@ export const defaultOptions = new Proxy<Options<any, any, any>>(
         set: (obj, key: string | symbol, val: unknown) => {
             if (!(key in optionsSchema)) {
                 obj[key] = val;
-                return true;
+                throw new TypeError(
+                    `Cannot define ${toSimpleString(key)} in deepIterate.options because is not defined in the schema\n` +
+                        `    The known options are:\n` +
+                        `      ${optionsList}\n`
+                );
             }
 
             const knownKey = key as keyof typeof optionsSchema;
