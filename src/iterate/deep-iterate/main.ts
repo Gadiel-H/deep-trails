Gadiel-H/deep-trails

commit 1afbc1cf3c7f4c2fc5cd53e8578fc68574e5ad8b

fix: prevent the modification of the "CallbackThis" object
Now, two "CallbackThis" objects with controlled mutation are used to improve security.

In addition, a read-only copy of the options is now created instead of using the original argument directly.

src/iterate/deep-iterate/main.ts
@@ -7,7 +7,7 @@ import type { Callback, Options, CoreParams, CallbackThis } from "../../types/de
 import { validateObject } from "../../__schemas/index.js";
 import { defaultOptions } from "./helpers/default-options.js";
 import { paramsSchema } from "./schemas/params-schema.js";
-import { toPathString } from "../../utils/public/index.js";
+import { toPathString, isPlainObject } from "../../utils/public/index.js";
 import { createLog } from "./helpers/log-creators.js";
 
 // ----- Core -----
@@ -64,21 +64,25 @@ export function deepIterate<R extends P, K = unknown, V = unknown, P extends obj
     callback: Callback<P, K, V, R> = () => {},
     options: Partial<Options<P, K, V>> = deepIterate.options
 ): CallbackThis<R, K, V, P> {
+    let optionsCopied = false;
+    let optionsCopy: Readonly<Options<P, K, V>> = options as any;
+
+    if (isPlainObject(options)) {
+        optionsCopy = { ...options } as any;
+        optionsCopied = true;
+    }
+
     validateObject(
-        { object, callback, options },
+        { object, callback, options: optionsCopy },
         paramsSchema,
         { options: deepIterate.options },
         "inputs in deepIterate"
     );
 
-    const optionsCopy = { ...options } as Options<P, K, V>,
-        { exposeVisitLog, visitLogType, callbackWrapper } = optionsCopy,
-        visitLog = createLog[visitLogType as any](),
-        params: CallbackThis<R, K, V, P> = {
-            root: object,
-            callback,
-            options
-        };
+    optionsCopy = Object.freeze((optionsCopied ? options : { ...options }) as Options<P, K, V>);
+
+    const { exposeVisitLog, visitLogType, callbackWrapper } = optionsCopy,
+        visitLog = createLog[visitLogType as any]();
 
     let callbackOrigin: CoreParams<P, K, V>["cbAlias"];
     let finalCallback = callback;
@@ -91,9 +95,22 @@ export function deepIterate<R extends P, K = unknown, V = unknown, P extends obj
         callbackOrigin = "The callback";
     }
 
-    finalCallback = finalCallback.bind(params);
+    const cbThis: CallbackThis<R, K, V, P> = Object.freeze({
+        root: object,
+        options: optionsCopy,
+        callback,
+        visitLog
+    });
+    const cbThisCopy = { ...cbThis };
 
-    if (exposeVisitLog) params.visitLog = visitLog;
+    if (exposeVisitLog) {
+        (cbThisCopy as any).visitLog = visitLog;
+    } else {
+        delete (cbThisCopy as any).visitLog;
+    }
+
+    Object.freeze(cbThisCopy);
+    finalCallback = finalCallback.bind(cbThisCopy);
 
     const { pathType } = optionsCopy,
         finishedSymbol = Symbol("FINISH"),
@@ -138,9 +155,7 @@ export function deepIterate<R extends P, K = unknown, V = unknown, P extends obj
         if (value !== finishedSymbol) throw value;
     }
 
-    params.visitLog = visitLog;
-
-    return params;
+    return cbThis;
 }
 
 Object.defineProperty(deepIterate, "options", {

src/types/deep-iterate/callback.ts
@@ -9,7 +9,7 @@ import type { ChildContext, ParentContext, Control, Options, VisitLog } from "./
  * - The return value is neither saved nor used.
  * - A synchronous callback is expected.
  *
- * @this {CallbackThis} The `this` object includes the received arguments.
+ * @this {CallbackThis} Includes the received arguments, and opcionally, a visit log.
  * @param child - Information about the current child.
  * @param parent - Information about the current parent.
  * @param control - Controls iteration behavior for the node and its parent.
@@ -28,26 +28,37 @@ export type Callback<P extends object, K = unknown, V = unknown, R extends P = a
 /**
  * `this` object bound to callbacks in `deepIterate`.
  *
- * - Includes the received arguments and the visit log (if enabled).
- * - This same object is returned at the end of the traverse.
+ * Includes the received arguments and the visit log (if enabled).
+ *
+ * Two objects of this type are created in `deepIterate`:
+ * - One is bound to the callback, and it may or may not include the visit log.
+ * - The other one is the one that is returned at the end of the traverse, and it includes the visit log.
  *
  * @since 3.0.0-beta.0
  */
-export type CallbackThis<R extends P, K = unknown, V = unknown, P extends object = object> = {
-    /** The root object passed to iterate it. */
+export type CallbackThis<
+    R extends P,
+    K = unknown,
+    V = unknown,
+    P extends object = object
+> = Readonly<{
+    /** The original root object passed to iterate it. */
     root: R;
 
     /** The original callback argument. */
     callback: Callback<P, K, V, R>;
 
-    /** The original options argument. */
-    options: Partial<Options<P, K, V>>;
+    /**
+     * A read-only copy of the original options argument.
+     */
+    options: Readonly<Options<P, K, V>>;
 
     /**
      * The optional visit log of parent nodes.
      *
-     * - Is undefined if `options.exposeVisitLog` is falsy, or null if there is no visit log.
+     * - Is undefined in the callback if `options.exposeVisitLog` is falsy.
+     * - Is null if `options.visitLogType` is `"null"`.
      * - It can always be accessed after finishing the traverse.
      */
     visitLog?: VisitLog<P, K> | null;
-};
+}>;

commit 52afd738850fe66f5ca85b0df705e6eb38c5a21a

fix: use the correct copy of the options in deepIterate
The bug was accidentally introduced in the previous commit (1afbc1cf).

Errors could occur if not all options were passed, as the incomplete object was used instead of the copy with all options.

src/iterate/deep-iterate/main.ts
@@ -79,7 +79,11 @@ export function deepIterate<R extends P, K = unknown, V = unknown, P extends obj
         "inputs in deepIterate"
     );
 
-    optionsCopy = Object.freeze((optionsCopied ? options : { ...options }) as Options<P, K, V>);
+    if (!optionsCopied) {
+        optionsCopy = { ...options } as Options<P, K, V>;
+    }
+
+    Object.freeze(optionsCopy);
 
     const { exposeVisitLog, visitLogType, callbackWrapper } = optionsCopy,
         visitLog = createLog[visitLogType as any]();