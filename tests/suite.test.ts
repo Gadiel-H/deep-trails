9eb7ac4c9202f7142aa42971419d6e491829f8e4
Gadiel Hernández <gadiel.dev@proton.me>
2026-08-27 00:03:02 -0700

    tests: fix the entry point

--- tests/suite.test.ts
+++ tests/suite.test.ts
@@ -1,6 +1,6 @@
 import { run } from "node:test";
 import { spec } from "node:test/reporters";
 
-const files = ["./tests/package/suite.test.ts", "./tests/cjs/suite.test.ts"];
+const files = ["./tests/package/suite.test.ts", "./tests/esm/suite.test.ts"];
 
 run({ files }).compose(new spec()).pipe(process.stdout);
